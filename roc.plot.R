9e2cf5ee7fb4d6a896010d7fba0569b17b8d2b3a
Michael Zhao <46555283+mzhao80@users.noreply.github.com>
2020-03-06 23:44:05 -0800

    Update roc.plot.R

--- roc.plot.R
+++ roc.plot.R
@@ -1,6 +1,5 @@
 setwd("Downloads/Data_science")
 roc = read.csv("ROC.score.csv")
-#write.csv(roc, "roc.score.csv")
 
 ggplot(roc, aes(x=Model.Name, y=ROC.AUC)) + geom_point() +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
